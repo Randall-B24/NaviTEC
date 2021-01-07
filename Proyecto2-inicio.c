bb77c1f03f1415ba4e688e2657764f3cbc4a41a0
Randall-B24 <76753453+Randall-B24@users.noreply.github.com>
2021-01-07 16:54:20 -0600

    Update Proyecto2-inicio.c

--- Proyecto2-inicio.c
+++ Proyecto2-inicio.c
@@ -3,6 +3,7 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <Windows.h> 
+#include <math.h>
 
 
 
@@ -381,8 +382,8 @@ typedef struct duende {
 typedef struct Lista2
 {
 	int largo;           // Largo de la lista doble
-	struct nino * inicio;
-	struct nino * final;
+	struct duende * inicio;
+	struct duende * final;
 } Lista_Doble2;
 
 // Crear una nueva lista doble
@@ -752,7 +753,191 @@ Lista_Doble2 * modificarLista_Doble2(Lista_Doble2 * registro_duendes){
 //                                  CÓDIGO PARA LOS JUGUETES
 /* ----------------------------------------------------------------------------------------------------------------------------*/
 
-int codigo_juguetes=1;
+
+
+/* ----------------------------------------------------------------------------------------------------------------------------*/ 
+//                                  CÓDIGO PARA LOS JUGUETES... ARBOL DE CODIGOS
+/* ----------------------------------------------------------------------------------------------------------------------------*/
+
+
+// Codigo para juguetes
+
+int valor_a=5;
+int valor_b=0;
+int valor_c=0;
+int pasar=0;
+
+
+
+
+/* Estructuras y tipos */
+typedef struct _nodo {
+   int codigo;
+   struct _nodo *derecho;
+   struct _nodo *izquierdo;
+} tipoNodo;
+
+typedef tipoNodo *pNodo;
+typedef tipoNodo *Arbol;
+
+
+/* Insertar un dato en el árbol ABB */
+void Insertar(Arbol *a, int dat)
+{
+   pNodo padre = NULL;
+   pNodo actual = *a;
+
+   /* Buscar el dato en el árbol, manteniendo un puntero al nodo padre */
+   while(!Vacio(actual) && dat != actual->codigo) {
+      padre = actual;
+      if(dat < actual->codigo) actual = actual->izquierdo;
+      else if(dat > actual->codigo) actual = actual->derecho;
+   }
+
+   /* Si se ha encontrado el elemento, regresar sin insertar */
+   if(!Vacio(actual)) return;
+   /* Si padre es NULL, entonces el árbol estaba vacío, el nuevo nodo será
+      el nodo raiz */
+   if(Vacio(padre)) {
+      *a = (Arbol)malloc(sizeof(tipoNodo));
+      (*a)->codigo = dat;
+      (*a)->izquierdo = (*a)->derecho = NULL;
+   }
+   /* Si el dato es menor que el que contiene el nodo padre, lo insertamos
+      en la rama izquierda */
+   else if(dat < padre->codigo) {
+      actual = (Arbol)malloc(sizeof(tipoNodo));
+      padre->izquierdo = actual;
+      actual->codigo = dat;
+      actual->izquierdo = actual->derecho = NULL;
+   }
+   /* Si el dato es mayor que el que contiene el nodo padre, lo insertamos
+      en la rama derecha */
+   else if(dat > padre->codigo) {
+      actual = (Arbol)malloc(sizeof(tipoNodo));
+      padre->derecho = actual;
+      actual->codigo = dat;
+      actual->izquierdo = actual->derecho = NULL;
+  }
+}
+
+/* Eliminar un elemento de un árbol ABB */
+void Borrar(Arbol *a, int dat)
+{
+   pNodo padre = NULL;
+   pNodo actual;
+   pNodo nodo;
+   int aux;
+
+   actual = *a;
+   /* Mientras sea posible que el valor esté en el árbol */
+   while(!Vacio(actual)) {
+      if(dat == actual->codigo) { /* Si el valor está en el nodo actual */
+         if(EsHoja(actual)) { /* Y si además es un nodo hoja: lo borramos */
+            if(padre) /* Si tiene padre (no es el nodo raiz) */
+               /* Anulamos el puntero que le hace referencia */
+               if(padre->derecho == actual) padre->derecho = NULL;
+               else if(padre->izquierdo == actual) padre->izquierdo = NULL;
+            free(actual); /* Borrar el nodo */
+            actual = NULL;
+            return;
+         }
+         else { /* Si el valor está en el nodo actual, pero no es hoja */
+            padre = actual;
+            /* Buscar nodo más izquierdo de rama derecha */
+            if(actual->derecho) {
+               nodo = actual->derecho;
+               while(nodo->izquierdo) {
+                  padre = nodo;
+                  nodo = nodo->izquierdo;
+               }
+            }
+            /* O buscar nodo más derecho de rama izquierda */
+            else {
+               nodo = actual->izquierdo;
+               while(nodo->derecho) {
+                  padre = nodo;
+                  nodo = nodo->derecho;
+               }
+            }
+            /* Intercambiar valores de no a borrar u nodo encontrado
+               y continuar, cerrando el bucle. El nodo encontrado no tiene
+               por qué ser un nodo hoja, cerrando el bucle nos aseguramos
+               de que sólo se eliminan nodos hoja. */
+            aux = actual->codigo;
+            actual->codigo = nodo->codigo;
+            nodo->codigo = aux;
+            actual = nodo;
+         }
+      }
+      else { /* Todavía no hemos encontrado el valor, seguir buscándolo */
+         padre = actual;
+         if(dat > actual->codigo) actual = actual->derecho;
+         else if(dat < actual->codigo) actual = actual->izquierdo;
+      }
+   }
+}
+
+/* Comprobar si un árbol es vacío */
+int Vacio(Arbol r)
+{
+   return r==NULL;
+}
+
+/* Comprobar si un nodo es hoja */
+int EsHoja(pNodo r)
+{
+   return !r->derecho && !r->izquierdo;
+}
+
+/* Función de prueba para recorridos del árbol */
+void Mostrar(int *d)
+{
+   printf("%d, ", *d);
+}
+
+
+/* Recorrido de árbol en inorden, aplicamos la función func, que tiene
+   el prototipo:
+   void func(int*);
+*/
+void InOrden(Arbol a, void (*func)(int*))
+{
+   if(a->izquierdo) InOrden(a->izquierdo, func);
+   func(&(a->codigo));
+   if(a->derecho) InOrden(a->derecho, func);
+}
+
+
+// Función sacar prioridad2 de las aignaciones
+int sacar_codigo(){
+	int valor;
+	
+	if (pasar==0){
+		valor=valor_a;
+		valor_a+=5;
+		pasar++;
+		}
+	else if (pasar==1){
+		valor= 3 * pow(2,valor_b);
+		valor_b++;
+		pasar++;
+		}
+	else{
+		valor= 2 * pow(2,valor_c);
+		valor_c++;
+		pasar=0;
+		}
+    
+    return valor;  // retornar valor 
+}
+
+
+
+
+/* ----------------------------------------------------------------------------------------------------------------------------*/ 
+//                                  CÓDIGO PARA LOS JUGUETES... LISTA DE DATOS
+/* ----------------------------------------------------------------------------------------------------------------------------*/
 
 // Estructura de los nodos
 typedef struct juguete {
@@ -763,25 +948,37 @@ typedef struct juguete {
     int edadmenor;
     int edadmayor;
     int costo;
-    struct juguete * izq;
-    struct juguete * der;
+    struct juguete * siguiente;
+    struct juguete * anterior;
 }Juguetes;
 
 
-//Crear raíz de árbol
+// Estructura de la lista doble
+typedef struct Lista3
+{
+	int largo;           // Largo de la lista doble
+	struct juguete * inicio;
+	struct juguete * final;
+} Lista_Doble3;
 
-Juguetes * crear_raiz(Juguetes * registro_juguetes){
-	registro_juguetes=NULL;
-	return registro_juguetes;
+
+// Crear una nueva lista doble
+Lista_Doble3 *listaNueva3(Lista_Doble3 * L)
+{
+	L= NULL;
+	L = (Lista_Doble3 *) malloc(sizeof(Lista_Doble3));
+	L->inicio = NULL;    // Poner inicio NULL
+	L->final = NULL;	// Poner final NULL
+	L->largo=0;			// Poner largo 0
+	return L;
 }
 
+// Crear un nuevo nodo para la lista doble
 
-// Insertar elementos en el arbol
-Juguetes * insertar_juguetes(Juguetes * registro_juguetes,struct juguete datos)
+Juguetes * nuevo_juguete (struct juguete datos)
 {
-    Juguetes * nuevo;
-    nuevo = malloc(sizeof(struct juguete));
-    
+	Juguetes * nuevo;
+	nuevo = (Juguetes *) malloc(sizeof(Juguetes));
     nuevo->codigo= datos.codigo;
     strcpy(nuevo->nombre,datos.nombre);
     strcpy(nuevo->descripcion,datos.descripcion);
@@ -789,43 +986,63 @@ Juguetes * insertar_juguetes(Juguetes * registro_juguetes,struct juguete datos)
     nuevo->edadmenor= datos.edadmenor;
     nuevo->edadmayor=datos.edadmayor;
     nuevo->costo=datos.costo;
-    nuevo->izq = NULL;
-    nuevo->der = NULL;
-    
-    
-    if (registro_juguetes == NULL){
-        registro_juguetes = nuevo;
-		}
-    else
-    {
-        Juguetes *anterior, *aux;
-        anterior = NULL;
-        aux = registro_juguetes;
-        while (aux != NULL)
-        {
-            anterior = aux;
-            if (datos.costo < aux->costo)
-                aux = aux->izq;
-            else
-                aux = aux->der;
-        }
-        if (datos.costo < anterior->costo)
-            anterior->izq = nuevo;
-        else
-            anterior->der = nuevo;
-    }
-	return registro_juguetes;    
+    nuevo->siguiente = NULL;
+    nuevo->anterior = NULL;
+	
+	return nuevo;            // retornar los datos
+}
+
+// Muestra la lista doble de derecha a izquierda
+void mostrarLista_Doble_Juguetes(Lista_Doble3 *Lista)
+{
+	Juguetes *i;
+	printf("Existen los siguientes juguetes en el sistema: \n");
+	for(i = Lista->inicio; i!= NULL; i = i->siguiente)               //recorrer la lista
+		printf("-Nombre: %s -Codigo: %i -Categoria: %s\n",i->nombre,i->codigo,i->categoria);   //Imprimir los datos
+	printf("\n");
+}
+
+// Muestra la lista doble de derecha a izquierda
+void mostrarLista_Doble_Juguetes2(Lista_Doble3 *Lista)
+{
+	Juguetes *i;
+	int contador=1;
+	printf("Existen los siguientes juguetes en el sistema: \n");
+	for(i = Lista->inicio; i!= NULL; i = i->siguiente){               //recorrer la lista
+		printf("%i-Nombre: %s \n",contador,i->nombre); //Imprimir los datos
+		contador++;  
+	}
+	printf("\n");
 }
 
 
-// Ingresar los datos de un nuevo juguete
-Juguetes * nuevo_juguete (Juguetes * registro_juguetes){
+
+// Insertar datos en la lista doble
+Lista_Doble3 * insertarDatos3(Lista_Doble3 * Lista, struct juguete datos)
+{
+	Lista->largo=Lista->largo+1;
+	if(Lista->inicio == NULL) //Valida si la lista está vacía
+	{
+		//Inserta al inicio de la lista
+		Lista->inicio= nuevo_juguete(datos);
+		Lista->final = Lista->inicio;
+		return Lista;
+	}
+	
+	//Inserta el dato al final de la lista, no necesita un ciclo porque tiene el puntero final
+	Lista->final->siguiente = nuevo_juguete(datos);
+	Lista->final->siguiente->anterior = Lista->final;
+	Lista->final = Lista->final->siguiente;
+}
+
+//Insertar datos de un juguete
+Lista_Doble3 * nuevos_datos3(Lista_Doble3 * registro_juguetes, int codigo_juguetes)
+{
 	int validar=0;
 	char term;
 	struct juguete datos;
 	
 	datos.codigo= codigo_juguetes;
-	codigo_juguetes++;
 	printf ("\nIngrese el nombre del juguete: ");
 	fflush (stdin);
 	gets(datos.nombre);
@@ -840,27 +1057,25 @@ Juguetes * nuevo_juguete (Juguetes * registro_juguetes){
 	while (validar==0){
 		printf ("\nIngrese la edad minima: ");
 		fflush (stdin);
-		if(scanf("%d%c", &datos.edadmenor, &term) != 2 || term != "\n"){
-			if (datos.edadmenor==0){
-    		color_error();
+		if(scanf("%d%c", &datos.edadmenor, &term) != 2 || term !='\n'){
+			color_error();
         	printf("\nIngrese un valor valido...\n");
-        	color_normal();				
-			}
-			else{
-    		validar++;
-    		}
+        	color_normal();    	
     	}
     	else{
-    		color_error();
-        	printf("\nIngrese un valor valido...\n");
-        	color_normal();
+    		validar++;
     		}
 	}
 	validar=0;
 	while (validar==0){
 		printf ("\nIngrese la edad maxima: ");
 		fflush (stdin);
-		if(scanf("%d%c", &datos.edadmayor, &term) != 2 || term != "\n"){
+		if(scanf("%d%c", &datos.edadmayor, &term) != 2 || term != '\n'){
+    		color_error();
+        	printf("\nIngrese un valor valido...\n");
+        	color_normal();
+    	}
+    	else{
 			if (datos.edadmayor==0){
     		color_error();
         	printf("\nIngrese un valor valido...\n");
@@ -869,18 +1084,18 @@ Juguetes * nuevo_juguete (Juguetes * registro_juguetes){
 			else{
     		validar++;
     		}
-    	}
-    	else{
-    		color_error();
-        	printf("\nIngrese un valor valido...\n");
-        	color_normal();
     		}
 	}
 	validar=0;
 	while (validar==0){
 		printf ("\nIngrese el costo del juguete: ");
 		fflush (stdin);
-		if(scanf("%d%c", &datos.costo, &term) != 2 || term != "\n"){
+		if(scanf("%d%c", &datos.costo, &term) != 2 || term != '\n'){
+    		color_error();
+        	printf("\nIngrese un valor valido...\n");
+        	color_normal();
+    	}
+    	else{
 			if (datos.costo==0){
     		color_error();
         	printf("\nIngrese un valor valido...\n");
@@ -890,34 +1105,67 @@ Juguetes * nuevo_juguete (Juguetes * registro_juguetes){
     		printf("El costo es: %d",datos.costo);
     		validar++;
     		}
-    	}
-    	else{
-    		color_error();
-        	printf("\nIngrese un valor valido...\n");
-        	color_normal();
     		}
 	}
+	registro_juguetes= insertarDatos3(registro_juguetes,datos);
 	color_aceptado();
 	printf ("\nJuguete agregado correctamente...\n\n");
 	color_normal();
 	fflush(stdin);
-	registro_juguetes= insertar_juguetes(registro_juguetes,datos);
-	return registro_juguetes;
 }
 
+// Eliminar por nombre
 
-// Mostrar juguetes registrados
-
-void Mostrar_Arbol_juguetes(Juguetes * registro_juguetes)
-{
-    if (registro_juguetes != NULL)
-    {
-        Mostrar_Arbol_juguetes(registro_juguetes->izq);
-        printf("Nombre: %s  Categoria: %s   Costo: %d  Codigo: %i\n\n",registro_juguetes->nombre,registro_juguetes->categoria,registro_juguetes->costo,registro_juguetes->codigo);
-        Mostrar_Arbol_juguetes(registro_juguetes->der);
-    }
+Lista_Doble3 * eliminar_juguetes_nombre (Lista_Doble3 * L,int codigo){
+	Juguetes *i;
+	struct juguete datos;
+	Lista_Doble3 *L2= listaNueva3(L2);
+	for(i = L->inicio; i!= NULL; i = i->siguiente){
+	if (i->codigo!=codigo){
+		datos.codigo= i->codigo;
+		strcpy(datos.categoria,i->categoria);
+		datos.costo=i->costo;
+		datos.edadmayor=i->edadmayor;
+		datos.edadmenor=i->edadmenor;
+		strcpy(datos.descripcion,i->descripcion);
+		strcpy(datos.nombre,i->nombre);
+		L2= insertarDatos3(L2,datos);
+	}
+	}
+	return L2;
 }
 
+// Seleccionar el juguete a eliminar
+Lista_Doble3 * EliminarLista_Doble3(Lista_Doble3 * registro_juguetes, int * codigo_juguete_aux,Arbol *registro_codigos_juguetes){
+	int contador=1,validar=0, dato;
+	char nombre[40];
+	Juguetes *i;
+	printf("Existen los siguientes juguetes en el sistema: \n\n");
+	for(i = registro_juguetes->inicio; i!= NULL; i = i->siguiente){          //recorrer la lista
+		printf ("%i-Nombre: %s \n",contador,i->nombre);
+		contador++;	
+	}
+	while (validar==0){
+	printf("Ingrese el nombre del juguete a eliminar: ");
+	scanf_s("%s",nombre);
+	
+	for(i = registro_juguetes->inicio; i!= NULL; i = i->siguiente){          //recorrer la lista
+	if (strcmp(i->nombre,nombre)==0){
+		codigo_juguete_aux=i->codigo;
+		printf ("....................%i..............",	codigo_juguete_aux);
+		Borrar(&registro_codigos_juguetes,codigo_juguete_aux);
+		registro_juguetes=eliminar_juguetes_nombre (registro_juguetes,i->codigo);
+		color_aceptado();
+		printf ("\nJuguete eliminado correctamente...\n\n");
+		color_normal();
+		return registro_juguetes;
+	}
+	}
+	color_error();
+	printf ("\nIngrese una identificacion valida...\n\n");
+	color_normal();
+	}
+}
 
 
 /* ----------------------------------------------------------------------------------------------------------------------------*/ 
@@ -930,8 +1178,9 @@ main (){
 	
 	Lista_Doble * registro_ninos = listaNueva(registro_ninos);
 	Lista_Doble2 * registro_duendes = listaNueva2(registro_duendes);
-	Juguetes * registro_juguetes = crear_raiz(registro_juguetes);
-	int validar=0,eleccion;
+	Lista_Doble3 * registro_juguetes = listaNueva3(registro_juguetes);
+	Arbol registro_codigos_juguetes=NULL;
+	int validar=0,eleccion,codigo_juguete_aux;
 	
 	
 	//Menu principal con las opciones	
@@ -942,12 +1191,16 @@ main (){
 		printf ("1- Agregar nino\n");
 		printf ("2- Mostrar ninos registrados\n");
 		printf ("3- Modificar datos de un nino\n");
-		printf ("4- Agregar duende\n");
-		printf ("5- Mostrar ninos registrados\n");
-		printf ("6- Modificar datos de un duende\n");
-		printf ("7- Agregar un nuevo juguete\n");
-		printf ("8- Mostrar juguetes registrados\n");
-		printf ("9- Salir del sistema\n");
+		printf ("4- Eliminar nino\n");
+		printf ("5- Agregar duende\n");
+		printf ("6- Mostrar duendes registrados\n");
+		printf ("7- Modificar datos de un duende\n");
+		printf ("8- Eliminar duende\n");
+		printf ("9- Agregar un nuevo juguete\n");
+		printf ("10- Mostrar juguetes registrados\n");
+		printf ("11- Modificar datos de un juguete\n");
+		printf ("12- Eliminar un juguete\n");
+		printf ("13- Salir del sistema\n");
 		
 		printf ("\nElija su opcion: ");
 		scanf(" %i", &eleccion);  //eleccion de la opcion a realizar 
@@ -971,21 +1224,37 @@ main (){
 				registro_ninos=modificarLista_Doble(registro_ninos);
 				break;
 			case 4:
-				nuevos_datos2(registro_duendes);
+				
 				break;
 			case 5:
-				mostrarLista_Doble_Duendes(registro_duendes);
+				nuevos_datos2(registro_duendes);
 				break;
 			case 6:
-				registro_duendes=modificarLista_Doble2(registro_duendes);
+				mostrarLista_Doble_Duendes(registro_duendes);
 				break;
-			case 7:
-				registro_juguetes=nuevo_juguete (registro_juguetes);
+			case 7:	
+				registro_duendes=modificarLista_Doble2(registro_duendes);   			
 				break;
 			case 8:
-				Mostrar_Arbol_juguetes(registro_juguetes);
+
 				break;
 			case 9:
+				codigo_juguete_aux= sacar_codigo();
+   				Insertar(&registro_codigos_juguetes,codigo_juguete_aux);
+   				nuevos_datos3(registro_juguetes,codigo_juguete_aux);
+				break;
+			case 10:
+				mostrarLista_Doble_Juguetes(registro_juguetes);
+				printf ("\n\n");
+				InOrden(registro_codigos_juguetes, Mostrar);
+				break;
+			case 11:
+				
+				break;
+			case 12:
+   				registro_juguetes=EliminarLista_Doble3(registro_juguetes,&codigo_juguete_aux,registro_codigos_juguetes);
+				break;
+			case 13:
 				color_aceptado();
 				printf ("\nGracias por utilizar el sistema...\n");  //Salir del sistema
 				color_normal(); 
